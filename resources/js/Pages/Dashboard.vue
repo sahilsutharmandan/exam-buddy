abe6f8900f5364a1094a11a086aae7f236152515
sahil <sahilsutharmandan@gmail.com>
2023-08-25 16:21:10 +0530

    Dashboard fixed.

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,13 @@
 <template>
     <AuthenticatedLayout>
         <div>
+            <div class="px-4 pb-6 mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <h2
+                    class="max-w-2xl mx-auto text-base font-semibold leading-6 text-gray-900 lg:mx-0 lg:max-w-none"
+                >
+                    Student Performance Overview
+                </h2>
+            </div>
             <div
                 class="border-b border-b-gray-900/10 lg:border-t lg:border-t-gray-900/5"
             >
@@ -78,150 +85,109 @@
                         Recent activity
                     </h2>
                 </div>
-                <div class="mt-6 overflow-hidden border-t border-gray-100">
-                    <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                        <div class="max-w-2xl mx-auto lg:mx-0 lg:max-w-none">
-                            <table class="w-full text-left">
-                                <thead class="sr-only">
-                                    <tr>
-                                        <th>Amount</th>
-                                        <th class="hidden sm:table-cell">
-                                            Client
-                                        </th>
-                                        <th>More details</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <template
-                                        v-for="day in days"
-                                        :key="day.dateTime"
-                                    >
-                                        <tr
-                                            class="text-sm leading-6 text-gray-900"
-                                        >
+                <div class="flow-root mx-8 lg:mt-8">
+                    <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                        <div
+                            class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8"
+                        >
+                            <div
+                                class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg"
+                            >
+                                <table
+                                    class="min-w-full divide-y divide-gray-300"
+                                >
+                                    <thead class="bg-gray-50">
+                                        <tr>
                                             <th
-                                                scope="colgroup"
-                                                colspan="3"
-                                                class="relative py-2 font-semibold isolate"
+                                                scope="col"
+                                                class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
                                             >
-                                                <time
-                                                    :datetime="day.dateTime"
-                                                    >{{ day.date }}</time
+                                                Name
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                            >
+                                                Date
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                            >
+                                                Subject
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                            >
+                                                Score
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                            >
+                                                Percentile
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                            >
+                                                Duration
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                            >
+                                                Correct Answers
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                            >
+                                                Incorrect Answers
+                                            </th>
+
+                                            <th
+                                                scope="col"
+                                                class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                                            >
+                                                Unanswered
+                                            </th>
+                                            <th
+                                                scope="col"
+                                                class="relative py-3.5 pl-3 pr-4 sm:pr-6"
+                                            >
+                                                <span class="sr-only"
+                                                    >Review Status</span
                                                 >
-                                                <div
-                                                    class="absolute inset-y-0 w-screen border-b border-gray-200 right-full -z-10 bg-gray-50"
-                                                />
-                                                <div
-                                                    class="absolute inset-y-0 left-0 w-screen border-b border-gray-200 -z-10 bg-gray-50"
-                                                />
                                             </th>
                                         </tr>
-                                        <tr
-                                            v-for="transaction in day.transactions"
-                                            :key="transaction.id"
-                                        >
-                                            <td class="relative py-5 pr-6">
-                                                <div class="flex gap-x-6">
-                                                    <component
-                                                        :is="transaction.icon"
-                                                        class="flex-none hidden w-5 h-6 text-gray-400 sm:block"
-                                                        aria-hidden="true"
-                                                    />
-                                                    <div class="flex-auto">
-                                                        <div
-                                                            class="flex items-start gap-x-3"
-                                                        >
-                                                            <div
-                                                                class="text-sm font-medium leading-6 text-gray-900"
-                                                            >
-                                                                {{
-                                                                    transaction.amount
-                                                                }}
-                                                            </div>
-                                                            <div
-                                                                :class="[
-                                                                    statuses[
-                                                                        transaction
-                                                                            .status
-                                                                    ],
-                                                                    'rounded-md py-1 px-2 text-xs font-medium ring-1 ring-inset',
-                                                                ]"
-                                                            >
-                                                                {{
-                                                                    transaction.status
-                                                                }}
-                                                            </div>
-                                                        </div>
-                                                        <div
-                                                            v-if="
-                                                                transaction.tax
-                                                            "
-                                                            class="mt-1 text-xs leading-5 text-gray-500"
-                                                        >
-                                                            {{
-                                                                transaction.tax
-                                                            }}
-                                                            tax
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                                <div
-                                                    class="absolute bottom-0 w-screen h-px bg-gray-100 right-full"
-                                                />
-                                                <div
-                                                    class="absolute bottom-0 left-0 w-screen h-px bg-gray-100"
-                                                />
-                                            </td>
+                                    </thead>
+                                    <tbody
+                                        class="bg-white divide-y divide-gray-200"
+                                    >
+                                        <tr>
                                             <td
-                                                class="hidden py-5 pr-6 sm:table-cell"
+                                                v-for="person in people"
+                                                :key="person"
+                                                class="py-4 pl-4 pr-3 text-sm font-medium text-gray-900 whitespace-nowrap sm:pl-6"
                                             >
-                                                <div
-                                                    class="text-sm leading-6 text-gray-900"
-                                                >
-                                                    {{ transaction.client }}
-                                                </div>
-                                                <div
-                                                    class="mt-1 text-xs leading-5 text-gray-500"
-                                                >
-                                                    {{
-                                                        transaction.description
-                                                    }}
-                                                </div>
+                                                {{ person }}
                                             </td>
-                                            <td class="py-5 text-right">
-                                                <div class="flex justify-end">
-                                                    <a
-                                                        :href="transaction.href"
-                                                        class="text-sm font-medium leading-6 text-indigo-600 hover:text-indigo-500"
-                                                        >View<span
-                                                            class="hidden sm:inline"
-                                                        >
-                                                            transaction</span
-                                                        ><span class="sr-only"
-                                                            >, invoice #{{
-                                                                transaction.invoiceNumber
-                                                            }},
-                                                            {{
-                                                                transaction.client
-                                                            }}</span
-                                                        ></a
-                                                    >
-                                                </div>
-                                                <div
-                                                    class="mt-1 text-xs leading-5 text-gray-500"
+
+                                            <td
+                                                class="relative py-4 pl-3 pr-4 text-sm font-medium text-right whitespace-nowrap sm:pr-6"
+                                            >
+                                                <a
+                                                    href="#"
+                                                    class="text-indigo-600 hover:text-indigo-900"
+                                                    >Edit</a
                                                 >
-                                                    Invoice
-                                                    <span class="text-gray-900"
-                                                        >#{{
-                                                            transaction.invoiceNumber
-                                                        }}</span
-                                                    >
-                                                </div>
                                             </td>
                                         </tr>
-                                    </template>
-                                </tbody>
-                            </table>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -234,7 +200,7 @@
                         <h2
                             class="text-base font-semibold leading-7 text-gray-900"
                         >
-                            Recent clients
+                            Purchased Courses
                         </h2>
                         <a
                             href="#"
@@ -424,62 +390,16 @@ const statuses = {
     Withdraw: "text-gray-600 bg-gray-50 ring-gray-500/10",
     Overdue: "text-red-700 bg-red-50 ring-red-600/10",
 };
-const days = [
-    {
-        date: "Today",
-        dateTime: "2023-03-22",
-        transactions: [
-            {
-                id: 1,
-                invoiceNumber: "00012",
-                href: "#",
-                amount: "$7,600.00 USD",
-                tax: "$500.00",
-                status: "Paid",
-                client: "Reform",
-                description: "Website redesign",
-                icon: ArrowUpCircleIcon,
-            },
-            {
-                id: 2,
-                invoiceNumber: "00011",
-                href: "#",
-                amount: "$10,000.00 USD",
-                status: "Withdraw",
-                client: "Tom Cook",
-                description: "Salary",
-                icon: ArrowDownCircleIcon,
-            },
-            {
-                id: 3,
-                invoiceNumber: "00009",
-                href: "#",
-                amount: "$2,000.00 USD",
-                tax: "$130.00",
-                status: "Overdue",
-                client: "Tuple",
-                description: "Logo design",
-                icon: ArrowPathIcon,
-            },
-        ],
-    },
-    {
-        date: "Yesterday",
-        dateTime: "2023-03-21",
-        transactions: [
-            {
-                id: 4,
-                invoiceNumber: "00010",
-                href: "#",
-                amount: "$14,000.00 USD",
-                tax: "$900.00",
-                status: "Paid",
-                client: "SavvyCal",
-                description: "Website redesign",
-                icon: ArrowUpCircleIcon,
-            },
-        ],
-    },
+const people = [
+    "Lindsay Walton",
+    "Front-end Developer",
+    "lindsay.walton@example.com",
+    "lindsay.walton@example.com",
+    "lindsay.walton@example.com",
+    "lindsay.walton@example.com",
+
+    "Member",
+    "Member",
 ];
 const clients = [
     {
